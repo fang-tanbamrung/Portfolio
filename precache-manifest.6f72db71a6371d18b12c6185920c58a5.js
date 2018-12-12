f867f304d8e5fefb2f6e423b48bb75fe65d23f10
fang-tanbamrung <tanbamrung_w@hotmail.com>
2018-12-12 16:38:56 +0700

    Updates

---
+++ precache-manifest.6f72db71a6371d18b12c6185920c58a5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "60d3ccd0aef46748ba7750cdfcf8b82e",
+    "url": "/Portfolio/static/media/todo.60d3ccd0.PNG"
+  },
+  {
+    "revision": "2c1ceaca87cdc94c3f79",
+    "url": "/Portfolio/static/css/main.9c309b6e.chunk.css"
+  },
+  {
+    "revision": "d638bd34b425dec39792d4a81189eddf",
+    "url": "/Portfolio/static/media/express.d638bd34.png"
+  },
+  {
+    "revision": "031ce7bff9fcb0420b05",
+    "url": "/Portfolio/static/js/1.031ce7bf.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "f5f73168f689d0bea342d2c5ff076000",
+    "url": "/Portfolio/static/media/email.f5f73168.png"
+  },
+  {
+    "revision": "ab1eeceba81900e4a11b1a7d1e5605bb",
+    "url": "/Portfolio/static/media/location.ab1eeceb.png"
+  },
+  {
+    "revision": "f38aac822ac7e278998bd81a15478719",
+    "url": "/Portfolio/static/media/robofriends.f38aac82.PNG"
+  },
+  {
+    "revision": "ec8737a6c4dfa0cc94621ae5a5f55403",
+    "url": "/Portfolio/static/media/facerecog.ec8737a6.PNG"
+  },
+  {
+    "revision": "2c1ceaca87cdc94c3f79",
+    "url": "/Portfolio/static/js/main.2c1ceaca.chunk.js"
+  },
+  {
+    "revision": "b1628428af58e40f30bc57c8bb9bc669",
+    "url": "/Portfolio/static/media/css.b1628428.png"
+  },
+  {
+    "revision": "2b290fdeed9f154f95fea558d73f3fa2",
+    "url": "/Portfolio/static/media/html.2b290fde.png"
+  },
+  {
+    "revision": "b91dd13ec7b9d2dcb62aa6f62c419b18",
+    "url": "/Portfolio/static/media/node.b91dd13e.png"
+  },
+  {
+    "revision": "6f2295825fe92a8787f36d707a31362c",
+    "url": "/Portfolio/static/media/mongodb.6f229582.png"
+  },
+  {
+    "revision": "8eaea1efde0be83c4b95699fd15dfe7d",
+    "url": "/Portfolio/static/media/postgres.8eaea1ef.png"
+  },
+  {
+    "revision": "f883245f38630f5c4d5bab2e907ea88b",
+    "url": "/Portfolio/static/media/react.f883245f.png"
+  },
+  {
+    "revision": "b882387a5a2b494c2c200e66d63fa8db",
+    "url": "/Portfolio/static/media/redux.b882387a.png"
+  },
+  {
+    "revision": "031ce7bff9fcb0420b05",
+    "url": "/Portfolio/static/css/1.f5a66771.chunk.css"
+  },
+  {
+    "revision": "e87f3fc5a83c6b9e737dc9fa2e13cf0c",
+    "url": "/Portfolio/index.html"
+  }
+];
