83ede512c928937fd76570526f8c1183114d0980
fang-tanbamrung <tanbamrung_w@hotmail.com>
2019-01-06 15:40:30 +0700

    change some information

--- src/component/body/Skills/skills.js
+++ src/component/body/Skills/skills.js
@@ -16,11 +16,11 @@ export const Skills = () => {
             <div  data-aos='zoom-in'>
                 <h2 className='bb pb2 text-shadow'>{'Skills'}</h2>
                 <div className='flex flex-wrap justify-center pa3 ma2'>
-                    <Skillbar percent='70%' pic={cssLogo} name={'CSS'}/>
+                    <Skillbar percent='60%' pic={cssLogo} name={'CSS'}/>
                     <Skillbar percent='70%' pic={htmlLogo} name={'html'}/>
-                    <Skillbar percent='70%' pic={jsLogo} name={'JavaScript'}/>
+                    <Skillbar percent='60%' pic={jsLogo} name={'JavaScript'}/>
                     <Skillbar percent='70%' pic={reactLogo} name={'React'}/>
-                    <Skillbar percent='50%' pic={reduxLogo} name={'Redux'}/>
+                    <Skillbar percent='60%' pic={reduxLogo} name={'Redux'}/>
                     <Skillbar percent='50%' pic={nodeLogo} name={'Node.js'}/>
                     <Skillbar percent='50%' pic={expressLogo} name={'expressjs'}/>
                     <Skillbar percent='70%' pic={postgresLogo} name={'PostgreSQL'}/>
